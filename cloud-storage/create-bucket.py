03846793f819d44dbe2941249320f0b62b8a0c64
Juan Tobar <juanpablococtm@gmail.com>
2026-01-08 18:18:14 -0500

    create bucket in gcs script

---
+++ cloud-storage/create-bucket.py
@@ -0,0 +1,19 @@
+import argparse
+from google.cloud import storage
+# Create a GCP bucket
+def main():
+  parser = argparse.ArgumentParser(description="Create a GCP bucket")
+  parser.add_argument("bucket_name", type=str, help="Name of the bucket to create")
+  args = parser.parse_args()
+  
+  bucket_name = args.bucket_name
+  print(f"Bucket name received: {bucket_name}")
+  # Initialize a storage client
+  storage_client = storage.Client()
+  bucket = storage_client.bucket(bucket_name)
+  bucket.storage_class = "STANDARD"
+  new_bucket = storage_client.create_bucket(bucket, location="us-central1")
+  print(f"Bucket {new_bucket.name} created in {new_bucket.location} with storage class {new_bucket.storage_class}")
+  
+if __name__ == "__main__":
+  main()
